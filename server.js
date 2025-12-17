a4d5b47eb4f8f6e3c777fcdf9ad34671a50dc635
Antony Peña <antonypenamejia2705@gmail.com>
2025-12-17 18:44:04 -0500

    Proyecto Revisado

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const PUERTO = process.env.PORT || 8080;
 const MONGO_URL = process.env.MONGO_URL;
 
 if (!MONGO_URL) {
-    console.error("❌ ERROR: MONGO_URL NO DEFINIDO EN .env");
+    console.error("ERROR: MONGO_URL NO DEFINIDO EN .env");
     process.exit(1);
 }
 
 mongoose.connect(MONGO_URL)
-    .then(() => console.log("✅ Conectado a MongoDB"))
-    .catch(err => console.error("❌ Error en Mongo:", err));
+    .then(() => console.log("Conectado a MongoDB"))
+    .catch(err => console.error("Error en Mongo:", err));
 
 app.listen(PUERTO, () => {
     console.log(`🚀 Servidor funcionando en el puerto ${PUERTO}`);
--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("🔥 Conectado a Mongo Atlas");
+    console.log("Conectado a Mongo Atlas");
   } catch (error) {
-    console.error("❌ Error al conectar a Mongo:", error);
+    console.error("Error al conectar a Mongo:", error);
   }
 };
